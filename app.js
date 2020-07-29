9745906a541c4bfcf82399f0c4df567af2d02553
rtalmage <raymondtalmage@gmail.com>
2020-07-28 22:04:42 -0700

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+var faker = require("faker");
+
+var name = faker.name.findName();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req, res){
+    res.render("index.html", {"name":name});
+});
+
+app.get("/cpu", function(req, res){
+    res.render("cpu.html", {"name":name});
+});
+
+app.get("/gpu", function(req, res){
+    res.render("gpu.html", {"name":name});
+});
+
+app.get("/storage", function(req, res){
+    res.render("storage.html", {"name":name});
+});
+
+//server
+app.listen("8080", "127.0.0.1", function(){
+    console.log("Express server is running...");
+});
